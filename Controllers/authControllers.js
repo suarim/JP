5e718a21e98e4d2421caccccaaa993ec5eed03a0
suarim <muhammadsuarim@gmail.com>
2024-07-26 14:26:27 +0530

    auth middleware

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,14 +1,17 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../Models/User");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { generatetoken } = require("../utils.js");
 
 const logincontroller = asyncHandler(async (req,res)=>{
     const {email,password} = req.body
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
     if(user){
         const match = await bcrypt.compare(password,user.password)
+        user = await User.findOne({ email }).select("-password");
         if(match){
-            res.status(200).json({message:"signed in successfully"})
+            token = generatetoken(user._id,res)
+            res.status(200).json({message:"signed in successfully",token,user})
         }
         else{
         res.status(404).json({error:"invalid password or email"})
@@ -50,13 +53,15 @@ const registercontroller = asyncHandler(async (req, res) => {
     });
 
     await newUser.save();
-
-    res.status(201).json({
+    token = generatetoken(newUser._id,res)
+    user={
         firstName: newUser.firstName,
         lastName: newUser.lastName,
         email: newUser.email,
         profilePic: newUser.profilePic,
         mobile: newUser.mobile,
-    });
+       
+    }
+    res.status(201).json({user,token});
 });
 module.exports =  {logincontroller,registercontroller}
---
+++ utils.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const generatetoken = (userid, res) => {
+    const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
+        expiresIn: '15d'
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production"
+    });
+
+    return token;
+};
+
+module.exports = { generatetoken };
